b9c558427142edb8ddd48f244d3a11e368e4d124
anthonb711 <anthonb711@byui.edu>
2025-03-06 22:22:39 -0500

    removing /target dir and basic menus

--- webScraper/src/main.rs
+++ webScraper/src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{self, Write};
+mod utils;
+
+
+
 fn main() {
-    println!("Hello, world!");
+  let mut input = String::new();
+
+
+  //menu loop
+  loop{
+    println!("\n\nWelcome to the Church of Jesus Christ of Latter-Day Saints Newsroom scraper.");
+    println!("Any key except \"q\" will fetch new releases. \"q\" will exit.\n");
+    print!("Your choice: -> " );
+    io::stdout().flush().unwrap();
+    io::stdin()
+    .read_line(&mut input)
+    .expect("Error reading input.");
+
+    let user_input = input.trim();
+
+    if user_input.eq_ignore_ascii_case("q"){
+      // TODO: function call to fetch, parse and format data
+        println!("See you next time!");
+      break;
+    }
+    println!("\n\nHere are the three most recent new releases");
+    println!("\t* 1st HEADLINE");
+    println!("\t* 2nd HEADLINE");
+    println!("\t* 3rd HEADLINE\n");
+    break;
+  }
+ 
 }
